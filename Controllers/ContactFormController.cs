91a11fac0fcc9c465798c8f5e6d247313832916f
dim <dimitris.giokas1@gmail.com>
2021-05-17 15:42:59 +0200

    deleted default connection string

---
+++ Controllers/ContactFormController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TAO_Backend.Models;
+
+namespace TAO_Backend.Controllers
+{
+    public class ContactFormController: BaseApiController
+    {
+        private readonly DBContext _context;
+        
+        public ContactFormController(DBContext context)
+        {
+            _context = context; 
+        }
+        
+        [HttpPost]
+        public void PostContactForm([FromBody] ContactForm contactForm)
+        {
+            // send email to us using the body of the request
+            Console.WriteLine("Sending email...");
+            Console.WriteLine(contactForm.message);
+        }
+    }
+}
---
+++ Models/ContactForm.cs
@@ -0,0 +1,9 @@
+﻿namespace TAO_Backend.Models
+{
+    public class ContactForm
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string message { get; set; }
+    }
+}
