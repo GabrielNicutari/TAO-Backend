48d6c9182dda8346c12b91b4856d6c9d15dce01b
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2021-05-24 16:59:36 +0200

    Initial setup

---
+++ Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace TAO_Backend.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public string Bio { get; set; }
+    }
+}
