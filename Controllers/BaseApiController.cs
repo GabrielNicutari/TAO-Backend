f3491a97c6d0a9ba94e8da71ddf7c3c66f7a0830
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2021-06-03 20:31:22 +0200

    Fix base api endpoint

--- Controllers/BaseApiController.cs
+++ Controllers/BaseApiController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace TAO_Backend.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/api/[controller]")]
     public class BaseApiController: ControllerBase
     {
     }
